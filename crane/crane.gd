7f4f1c2ce4c1443d346884b154c198a2e98fc9d4
crazyrems <tauziac0remy+fork@gmail.com>
2023-06-24 00:39:19 +0200

    a commit?

--- crane/crane.gd
+++ crane/crane.gd
@@ -12,13 +12,13 @@ var _prev_bf_rate = 0
 func _physics_process(delta):
 	var turn_rate = _prev_turn_rate + clamp((Input.get_axis("left", "right") - _prev_turn_rate), -0.03, 0.03)
 	var motor_sound_level = abs(turn_rate)
-	$Foot/SoundMotor.pitch_scale = motor_sound_level
+	$Foot/SoundMotor.pitch_scale = max(0.1, motor_sound_level)
 	$Foot/SoundMotor.volume_db = -80 * (1 - min(1, motor_sound_level * 2))
 	$crane.rotate_y(-turn_rate * turn_speed * delta)
 	_prev_turn_rate = turn_rate
 	var back_forth_rate = _prev_bf_rate + clamp(Input.get_axis("backward", "forward") - _prev_bf_rate, -0.03, 0.03)
 	var rope_sound_level = abs(back_forth_rate)
-	$crane/SoundRope.pitch_scale = rope_sound_level
+	$crane/SoundRope.pitch_scale = max(0.1, rope_sound_level)
 	$crane/SoundRope.volume_db = -80 * (1 - min(1, rope_sound_level * 2))
 	$crane/HookScript.position.z = clamp($crane/HookScript.position.z - back_forth_rate * move_speed * delta, -max_move_pos, -min_move_pos)
 	_prev_bf_rate = back_forth_rate
